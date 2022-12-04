7e0acf91c82f7dba1884dda93b9f32eecb6d9799
daressel-after <o.chasovskoy@afterlogic.com>
2022-12-04 15:40:22 +0300

    Add codegen for graphql

---
+++ codegen.ts
@@ -0,0 +1,27 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: './src/**/*.graphql',
+  generates: {
+    'src/generated/graphql.ts': {
+      plugins: [
+        'typescript',
+        'typescript-resolvers',
+        'typescript-document-nodes',
+      ],
+    },
+    'lib/generated/graphql.ts': {
+      plugins: [
+        'typescript',
+        'typescript-resolvers',
+        'typescript-document-nodes',
+      ],
+    },
+    // './graphql.schema.json': {
+    //   plugins: ['introspection'],
+    // },
+  },
+};
+
+export default config;
